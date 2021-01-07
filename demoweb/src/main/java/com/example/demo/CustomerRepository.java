d43c4e17c519177c36b3c889f2e97555ded41311
Albert Huber <albert.huber@printus.de>
2021-01-07 15:16:33 +0100

    erweiterung um rest server /list

---
+++ demoweb/src/main/java/com/example/demo/CustomerRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class CustomerRepository {
+    List<Customer> customerList = new ArrayList<Customer>();
+
+    @PostConstruct
+    public void init(){
+        customerList.add(new Customer(1, "albert"));
+        customerList.add(new Customer(2, "maxim"));
+    }
+    public List<Customer> getData() {
+        return customerList;
+    }
+}
